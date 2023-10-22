2e6a60589cf37acac16296f98dbb53d2d1068def
BloodDragon2580 <gewinner.thomas@gmail.com>
2023-10-22 06:17:13 +0200

    Upload v1.0

---
+++ FastOpen/locale/deDE.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("FastOpen", "deDE")
+if not L then return end
+
+L["loading"] = "|cffd6266cFastOpen |cFFFFD700wurde geladen"
---
+++ FastOpen/locale/enUS.lua
@@ -0,0 +1,3 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("FastOpen", "enUS", true)
+
+L["loading"] = "|cffd6266cFastOpen |cFFFFD700loaded"
